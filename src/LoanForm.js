2bf5485048eb9cf2adc9e00888f71bde554beb41
Bader Zoabi <bader.zoabi@gmail.com>
2025-04-06 17:35:20 +0200

    Attach useState logic to the forms inputs

--- src/LoanForm.js
+++ src/LoanForm.js
@@ -1,7 +1,24 @@
+import { useState } from "react";
 import "./FormStyles.css";
 import Modal from "./Modal";
 
 export default function LoanForm() {
+  const [loanInputs, setLoanInputs] = useState({
+    name: "",
+    phoneNumber: "",
+    age: "",
+    isEmployee: false,
+    salary: "",
+  });
+
+  const handleLoanInputsChange = (event) => {
+    setLoanInputs({ ...loanInputs, [event.target.name]: event.target.value });
+  };
+
+  const handleLoanCheckboxChange = (event) => {
+    setLoanInputs({ ...loanInputs, isEmployee: event.target.checked });
+  };
+
   return (
     <div id="LoanFormContainer" className="flex">
       <form id="LoanForm" className="flex">
@@ -9,19 +26,43 @@ export default function LoanForm() {
         <hr></hr>
 
         <label>Name:</label>
-        <input type="text"></input>
+        <input
+          name="name"
+          type="text"
+          value={loanInputs.name}
+          onChange={handleLoanInputsChange}
+        ></input>
 
         <label>Phone Number:</label>
-        <input type="text"></input>
+        <input
+          name="phoneNumber"
+          type="text"
+          value={loanInputs.phoneNumber}
+          onChange={handleLoanInputsChange}
+        ></input>
 
         <label>Age:</label>
-        <input type="text"></input>
+        <input
+          name="age"
+          type="text"
+          value={loanInputs.age}
+          onChange={handleLoanInputsChange}
+        ></input>
 
         <label style={{ marginTop: "30px" }}>Are you an employee?</label>
-        <input type="checkbox" style={{ marginBottom: "10px" }}></input>
+        <input
+          type="checkbox"
+          style={{ marginBottom: "10px" }}
+          checked={loanInputs.isEmployee}
+          onChange={handleLoanCheckboxChange}
+        ></input>
 
         <label>Salary</label>
-        <select>
+        <select
+          name="salary"
+          value={loanInputs.salary}
+          onChange={handleLoanInputsChange}
+        >
           <option>Less than 500$</option>
           <option>Between 500$ amd 2000$</option>
           <option>Above 2000$</option>
@@ -30,7 +71,7 @@ export default function LoanForm() {
         <button id="submitBtn">Submit</button>
       </form>
 
-      <Modal />
+      {/* <Modal /> */}
     </div>
   );
 }
